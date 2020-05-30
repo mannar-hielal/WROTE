bcf81dafcf4ec17525e2323895e3ab5c76fd7b4a
vinous22 <adminwithu@gmail.com>
2020-05-30 16:45:05 +0200

    Connect to PostgreSQL database

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -15,11 +15,9 @@ import environ
 from django.utils.translation import ugettext_lazy
 from decouple import config
 
-
 env = environ.Env()
 if os.path.exists('.env'):
     environ.Env.read_env('.env')
-
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -92,12 +90,12 @@ WSGI_APPLICATION = 'blogsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+database_url = env(env.DEFAULT_DATABASE_ENV, default=None)
+if database_url:
+    default_database = env.db_url_config(database_url)
+    DATABASES = {
+        'default': default_database,
     }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
