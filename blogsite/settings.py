e19e282ed743b12d89c5389a6550a98d7e6c68cb
mannarhielal <adminwithu@gmail.com>
2020-09-09 08:58:25 +0200

    Update static settings

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -149,17 +149,17 @@ LOCALE_PATHS = [
 
 # static_root the absolute path to the directory where static files
 # from all app are collected in one place to be served in production
-STATIC_ROOT = env('STATIC_ROOT', default='/static_root/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
 
 # the url to use when referring to the static_root dir
-STATIC_URL = env('STATIC_URL', default='/static/')
+STATIC_URL = env('STATIC_URL', default='/static_url/')
 
 # BASE_DIR=WROTE, STATICFILES_DIRS = WROTE/static
 # project might have assets that are not tied to a particular
 # app, in addition to staic inside each app, we can define
 # list of directories where django can look for static files
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'download/'),
 ]
 
 # List of finder classes that know how to find static files in
